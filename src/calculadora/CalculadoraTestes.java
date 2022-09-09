f93fb256ca71c09aeb56322a1d5b3c7ed26a86e9
thalitaapsouza <thaapsouza@hotmail.com>
2022-09-09 18:30:59 -0300

    Arrumando comentarios

--- src/calculadora/CalculadoraTestes.java
+++ src/calculadora/CalculadoraTestes.java
@@ -13,85 +13,82 @@ public class CalculadoraTestes {
     public void TesteSoma() {
         int soma = 0;
 
+        //Cenario de teste 1: Soma de dois valores 
         CalculadoraOperacoes calc = new CalculadoraOperacoes();
         soma = calc.somar(2, 8);
         System.out.println(soma);
 
-        //Cenario de teste 1: Soma de dois valores 
+        //Cenario de testes 2: Soma de dois valores sendo um zero 
         soma = calc.somar(4, 0);
         System.out.println(soma);
-//Cenario de testes 2: Soma de dois valores sendo um zero 
 
+        //Cenario de teste 3: Soma de dois valores sendo que ambos sao zero
         soma = calc.somar(0, 0);
         System.out.println(soma);
 
-//Cenario de teste 3: Soma de dois valores sendo que ambos sao zero
+        //Cenario de teste 4: Soma de dois valores sendo um negativo
         soma = calc.somar(8, -2);
         System.out.println(soma);
-//Cenario de teste 4: Soma de dois valores sendo um negativo
     }
 
     public void TesteSubtracao() {
         int sub = 0;
 
         CalculadoraOperacoes calc = new CalculadoraOperacoes();
+        //Cenario de teste 1: Subtraçao de dois valores
         sub = calc.subtrair(10, 6);
         System.out.println(sub);
-        //Cenario de teste 1: Subtraçao de dois valores
-        
-         sub = calc.subtrair( 8, 0);
-        System.out.println(sub);
+
         //Cenario de testes 2: Subtraçao de dois valores sendo um zero 
-        
-            sub = calc.subtrair(0, 0);
+        sub = calc.subtrair(8, 0);
         System.out.println(sub);
+
         //Cenario de teste 3: Subtraçao de dois valores sendo que ambos sao zero
-  sub = calc.subtrair(8, -2);
+        sub = calc.subtrair(0, 0);
         System.out.println(sub);
         //Cenario de teste 4: Subtraçao de dois valores sendo um negativo
-        
+        sub = calc.subtrair(8, -2);
+        System.out.println(sub);
+
     }
+
     public void TesteMultiplicaçao() {
         int mult = 0;
 
-          CalculadoraOperacoes calc = new CalculadoraOperacoes();
+        CalculadoraOperacoes calc = new CalculadoraOperacoes();
+        //Cenario de teste 1: Multiplicacao de dois valores
         mult = calc.multiplicacao(45, 3);
         System.out.println(mult);
-        //Cenario de teste 1: Multiplicacao de dois valores
-        
-           mult = calc.multiplicacao( 20, 0);
+
+        //Cenario de testes 2: Multiplicacao de dois valores sendo um zero 
+        mult = calc.multiplicacao(20, 0);
         System.out.println(mult);
-                //Cenario de testes 2: Multiplicacao de dois valores sendo um zero 
-           mult = calc.multiplicacao(0, 0);
-        System.out.println(mult);
-         //Cenario de teste 3: Multiplicaçao de dois valores sendo que ambos sao zero
-         mult = calc.multiplicacao(2, -30);
+        //Cenario de teste 3: Multiplicaçao de dois valores sendo que ambos sao zero
+        mult = calc.multiplicacao(0, 0);
         System.out.println(mult);
         //Cenario de teste 4: Multiplicacao de dois valores sendo um negativo
-        
-               
- }
+        mult = calc.multiplicacao(2, -30);
+        System.out.println(mult);
+
+    }
+
     public void TesteDivisao() {
         double div = 0;
-        
-          CalculadoraOperacoes calc = new CalculadoraOperacoes();
+
+        CalculadoraOperacoes calc = new CalculadoraOperacoes();
+        //Cenario de teste 1: divisao de dois valores
         div = calc.divisao(9, 3);
         System.out.println(div);
-        //Cenario de teste 1: divisao de dois valores
-        
-           div = calc.divisao ( 0, 9);
+
+        //Cenario de testes 2: divisao de dois valores sendo um zero 
+        div = calc.divisao(0, 9);
         System.out.println(div);
-                //Cenario de testes 2: divisao de dois valores sendo um zero 
-           div = calc.divisao(0, 0);
-        System.out.println(div);
-         //Cenario de teste 3: divisao de dois valores sendo que ambos sao zero
-         div = calc.divisao(4, -2);
+        //Cenario de teste 3: divisao de dois valores sendo que ambos sao zero
+        div = calc.divisao(0, 0);
         System.out.println(div);
         //Cenario de teste 4: divisao de dois valores sendo um negativo
-    }  
-        
-         
+        div = calc.divisao(4, -2);
+        System.out.println(div);
+    }
 
-
-   }
-
+}
